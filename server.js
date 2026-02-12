12912d4dea1d53da7cb88fd900551cc53d5f2c62
shub200 <ryuk67678@gmail.com>
2026-02-12 10:56:47 +0530

    server error fixed

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const app = require("./index");
 
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Server is Running on' + PORT);
 });
